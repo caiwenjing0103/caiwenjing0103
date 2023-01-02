b5ebb411d892ca6b90ff18e0728fd90d99008592
caiwenjing0103 <121763818+caiwenjing0103@users.noreply.github.com>
2023-01-03 05:50:04 +0800

    Add files via upload

---
+++ src/settings.e6dee.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/New Folder/00.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",main:"aded0"}};
